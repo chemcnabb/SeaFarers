a5c485f79e50602830d94de87132eff3f78231f7
chemcnabb <chemcnabb@gmail.com>
2012-10-02 18:24:06 -0700

    Update game/msg.py

--- game/msg.py
+++ game/msg.py
@@ -1,1 +1,32 @@
+import pygame
+class Text(pygame.sprite.Sprite):
+    """ a helper class to write text on the screen """
+    number = 0
+    book = {}
+    def __init__(self, pos, msg):
+        self.number = Text.number # get a unique number
+        Text.number += 1 # prepare number for next Textsprite
+        Text.book[self.number] = self # store myself into the book
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = [0.0,0.0]
+        self.pos[0] = pos[0]
+        self.pos[1] = pos[1]
+        self.msg = msg
+        self.changemsg(msg)
 
+    def update(self, seconds):
+        pass
+
+    def changemsg(self,msg):
+        self.msg = msg
+        self.image = write(self.msg)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.pos[0]
+        self.rect.centery = self.pos[1]
+
+def write(msg="pygame is cool"):
+    """helper function for the Text sprite"""
+    myfont = pygame.font.SysFont("None", 28)
+    mytext = myfont.render(msg, True, (0,0,0))
+    mytext = mytext.convert_alpha()
+    return mytext
