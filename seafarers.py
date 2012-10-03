f6f02ab7ef6a0aa8432fcbe2427dbb10764020fc
unknown <cmcnabb@bcws225.d2vancouver.com>
2012-10-03 10:13:32 -0700

    adding up to date file

--- seafarers.py
+++ seafarers.py
@@ -30,6 +30,10 @@ clock=pygame.time.Clock()
 
 # -------- Main Program Loop -----------
 while not done:
+    milliseconds = clock.tick(100)  # milliseconds passed since last frame
+    seconds = milliseconds / 1000.0 # seconds passed since last frame (float)
+
+
     # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
@@ -48,16 +52,16 @@ while not done:
 
     # First, clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
-    screen.fill(white)
+    screen.fill((128,128,255))
     ship.draw()
-    ship.update()
+    ship.update(seconds)
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
 
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
     # Limit to 20 frames per second
-    clock.tick(20)
+    #clock.tick(100)
 
 # Close the window and quit.
 # If you forget this line, the program will 'hang'
